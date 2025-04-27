d73fbdbca7aa5b511d38b63f873f466b8c1d8703
Abdil Karim Bakir <abdilkarimb@gmail.com>
2025-04-27 13:43:20 +0200

    %IDE-First-Steps 0.5 h

---
+++ Basis/IDE/IDE-First-Steps.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+def get_permutations(string, i=0):
+    res = []
+    if i == len(string):
+        res.append(string)
+        
+    for j in range(i, len(string)):
+        chars = list(string)
+        chars[i], chars[j] = chars[j], chars[i]
+        res.extend(get_permutations("".join(chars), i + 1))
+    return res
+
+print(get_permutations("FUB"), end="\nHelloWorld\n")
