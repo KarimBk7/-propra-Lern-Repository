8f12b0a801e02a6ee9ab286677a390d20822b3b9
Abdil Karim Bakir <abdilkarimb@gmail.com>
2025-06-09 21:07:02 +0200

    %m_requests 1.0h

---
+++ Bibliotheken/pip-popular/m_requests.md
@@ -0,0 +1,8 @@
+### F1:
++ Da steht Statuscode '403' wird angezeigt, welches wohl 'forbidden' bedeuten soll.
++ Weiterleitung zu einer anderen Domäne
+
+### F2:
++ Beim ersten Befehl wird Statuscode '503' angezeigt, welches 'Service unavailabel' bedeutet.
++ Beim zweiten Befehl wird Statuscode '404' angezeigt, welches 'Not Found' bedeutet.
++ Die Zweite Seite wurde nicht gefunden, während die erste Seite normal gefunden wurde.
--- Bibliotheken/pip-popular/m_requests.py
+++ Bibliotheken/pip-popular/m_requests.py
@@ -1,11 +1,18 @@
 import requests
 import sys
+from charset_normalizer import from_bytes
+import json
 
 def do_get(url: str, searchstring: str):
 	r = requests.get(url)
 	print("Status: ",r.status_code)
 	print("received ",len(r.text),"characters for ",r.url)
 	
+	# K1 charset normalizer
+	detection = from_bytes(r.content).best()
+	r.encoding = detection.encoding if detection else r.encoding
+
+
 	print("Encoding: ", r.encoding)
 	print("Headers:")
 	for name, value in r.headers.items():
@@ -22,9 +29,19 @@ def do_get(url: str, searchstring: str):
 		print()
 
 
+def do_post(url: str, jsontext: str):
+	
+	x = requests.post(url, jsontext)
+	print("Status: ",x.status_code)
+	result = json.dumps(x.json(), indent=4)
+	print(result)
+	
+
+
 if __name__=="__main__":
 	if sys.argv[1] == "do_get":
 		#_, _, url, searchstring = sys.argv
 		do_get(sys.argv[2], sys.argv[3])
 
-#do_get("https://www.inf.fu-berlin.de/inst/ag-se/teaching/K-ProPra-2024-04/chapter-Basis.html","grundlegend")
+	elif sys.argv[1] == "do_post":
+		do_post(sys.argv[2], sys.argv[3])
