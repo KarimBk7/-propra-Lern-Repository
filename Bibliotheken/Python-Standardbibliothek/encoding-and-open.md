6af1364ce91ed9307a9b421e3fddae1bc01e06e5
Karim <abdilkarimb@gmail.com>
2025-06-11 15:33:50 +0200

    encoding und so

---
+++ Bibliotheken/Python-Standardbibliothek/encoding-and-open.md
@@ -0,0 +1,7 @@
+### F1: Welche Datei zu welchem Encoding
+|Datei|Encoding|
+|-----|--------|
+|Datei1|UTF-8|
+|Datei2|ISO-8859-1 und ISO-8859-9|
+|Datei3|cp500 und EBCDIC-CP-BE|
+|Datei4|ISO-8859-1|
---
+++ Bibliotheken/Python-Standardbibliothek/encoding-and-open.py
@@ -0,0 +1,10 @@
+
+filenames = [f"encoding/datei{i+1}" for i in range(4)]
+encodings = ['utf8', 'cp500', 'iso-8859-1', 'iso-8859-9', 'EBCDIC-CP-BE']
+
+for file in filenames:
+    for enc in encodings:
+        with open(file, mode="rt", encoding=enc, errors='replace') as f:
+            content = f.read()
+        print(f"Date: {file} mit {enc}: {content}")
+
