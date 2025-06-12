4b9787f7e520ff6eade08e8c702eb2a068cabc69
Karim <abdilkarimb@gmail.com>
2025-06-12 19:40:07 +0200

    %encoding-and-open 1.0h

--- Bibliotheken/Python-Standardbibliothek/encoding-and-open.py
+++ Bibliotheken/Python-Standardbibliothek/encoding-and-open.py
@@ -4,7 +4,9 @@ encodings = ['utf8', 'cp500', 'iso-8859-1', 'iso-8859-9', 'EBCDIC-CP-BE']
 
 for file in filenames:
     for enc in encodings:
-        with open(file, mode="rt", encoding=enc, errors='replace') as f:
+        #with open(file, mode="rt" ,errors="replace") as f:
+        with open(file, mode="rb") as f:
             content = f.read()
+            result = content.decode(enc, errors="replace")
         print(f"Date: {file} mit {enc}: {content}")
 
