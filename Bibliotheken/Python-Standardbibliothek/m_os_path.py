d34017b78f257aa694983657de2a4f5a381a8a7d
Abdil Karim Bakir <abdilkarib03@gmail.com>
2025-06-24 15:16:45 +0200

    m_os

--- Bibliotheken/Python-Standardbibliothek/m_os_path.py
+++ Bibliotheken/Python-Standardbibliothek/m_os_path.py
@@ -10,7 +10,7 @@ print("home directory exists: ", os.path.exists(homePath))
 
 # A3
 liste = os.listdir(homePath)
-print(liste)
+# print(liste)
 
 # A4
 for i in range (0,len(liste)):
@@ -31,11 +31,32 @@ for i in liste:
 print("# directories:", pathcount, ", # files:", filecount)
 
 # A6
-liste = [pfad for pfad in liste if os.path.isfile(pfad)]
+liste = [pfad for pfad in liste if os.path.isfile(pfad)]							# Alle Unterpfade entfernen. Nur Datein in Liste erlaubt
 
-biggestFile = max([os.path.getsize(pfad) for pfad in liste])
-lastcreated = time.localtime(max([os.path.getctime(pfad) for pfad in liste]))
-lastchanged = time.localtime(max([os.path.getmtime(pfad) for pfad in liste]))
-mostcommon  = " "
+biggestFile = max([os.path.getsize(pfad) for pfad in liste])						# groesste Datei
+lastcreated = time.localtime(max([os.path.getctime(pfad) for pfad in liste]))		# zuletzt erstellte Datei  (Linux: Zeitpunkt letzte Metadatenänderung)
+lastchanged = time.localtime(max([os.path.getmtime(pfad) for pfad in liste]))		# zuletzt geaenderte Datei
+
+filetype ={}
+for i in liste:
+	if os.path.splitext(i)[1] in filetype:
+		filetype[os.path.splitext(i)[1]] += 1
+	else:
+		filetype[os.path.splitext(i)[1]] = 1
+
+mostcommon = max(filetype, key=lambda ft: filetype[ft])									# am haeeufigsten vorkommende Dateiendung
 
 print("biggest file:", biggestFile, "\nlast created:", lastcreated, "\nlast changed:", lastchanged, "\nmost common file extension:", mostcommon)
+
+
+# A7
+cwd = os.getcwd()
+home = os.path.expanduser("~")
+rel_path = os.path.relpath(cwd,home)
+print("relative path from home to cwd: ",cwd)
+
+
+# A8
+back_path = os.path.relpath(home,cwd)
+comb = os.path.join(rel_path, back_path)
+print("path home to cwd and back: ", comb)
