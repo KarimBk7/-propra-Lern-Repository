884353713d5f4f3914e9a44337873aebe44bea28
Abdil Karim Bakir <abdilkarib03@zedat.fu-berlin.de>
2025-07-27 17:44:52 +0200

    m_hashlib 1.0 h

---
+++ Bibliotheken/Python-Standardbibliothek/m_hashlib.md
@@ -0,0 +1,4 @@
+### F1: Wie sicher sind MD5, SHA-1 und SHA-256
++ MD5: Wird nicht als sicher angesehen, weil es anfällig auf Kollisionen ist. Dadurch ist es schwierig Daten zu Authentifizieren.
++ SHA-1: Wie MD5 ist dieses ebenfalls anfällig für Kollisionen
++ SHA-256: Wird als sicher gesehen (keine Kollisionen), jedoch ist es aufgrund der schnellen Geschwindigkeit anfällig für Bruteforce-Attacken, wenn man damit Passwörter hasht.
---
+++ Bibliotheken/Python-Standardbibliothek/m_hashlib.py
@@ -0,0 +1,49 @@
+import hashlib
+
+# A1
+print(hashlib.algorithms_available)
+
+
+# A2
+h = hashlib.new("sha256")
+
+
+# A3
+h.update(b"ProPra")
+pp = h.hexdigest()
+print("1. ProPra SHA-256:\t", pp)
+
+
+# A4
+h = hashlib.new("sha256")
+h.update(b"ProPra ")
+pp_ = h.hexdigest()
+print("2. added a space:\t", pp_)
+
+
+# A5
+def compare(h1, h2):
+	found = 0
+	for i, j in zip(pp, pp_) :
+		if i == j:
+			found += 1
+	return found
+
+expected = 64 * (1/16)  # Hash erstellt 64 Hex-Zeichen und Hexadezimal hat 16 verschiedene Zeichen
+print("3. identical digits: expected: ", expected , "; found: ", compare(pp, pp_))
+
+
+# A6
+h2 = hashlib.new("sha256")
+h2.update(b"ProPra FU Berlin")
+h.update(b"FU Berlin")			# Aktuell "ProPra "
+print("4. u(a); u(b) = u(a + b):\t", h.hexdigest() == h2.hexdigest())
+
+
+# A8
+with open("m_hashlib.txt", mode="rb") as f:
+    content = f.read()
+
+h3 = hashlib.new("sha256")
+h3.update(b""+content)
+print("5. file checksum:\t", h3.hexdigest())
