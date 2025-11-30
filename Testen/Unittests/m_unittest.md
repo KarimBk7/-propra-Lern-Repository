0fd6fc80be064367142819c97d6c7b29c63c831a
Abdil Karim Bakir <abdilkarib03@zedat.fu-berlin.de>
2025-11-30 23:13:59 +0100

    unittest begin

---
+++ Testen/Unittests/m_unittest.md
@@ -0,0 +1,4 @@
+### F1: Was bedeutet vermutlich der Punkt in der ersten Zeile der Ausgabe?
+Der Punkt bedeutet: ``Test bestanden``
+
+
--- Testen/Unittests/test_unittest.py
+++ Testen/Unittests/test_unittest.py
@@ -1,6 +1,10 @@
 import unittest
+import math
 
 class Test(unittest.TestCase):
 
 	def test_addition(self):
 		self.assertEqual(1+1, 2) 
+
+	def test_sqrt(self):
+		self.assertEqual(math.sqrt(10)**10, 10)
