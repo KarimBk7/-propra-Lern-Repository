f6e2f6c9be33ffb521514a9a3886bf7b57081712
Abdil-Karim Bakir <abdilkarib03@zedat.fu-berlin.de>
2025-12-01 11:31:54 +0100

    unittest

--- Testen/Unittests/test_unittest.py
+++ Testen/Unittests/test_unittest.py
@@ -6,6 +6,7 @@ class Test(unittest.TestCase):
 	def test_addition(self):
 		self.assertEqual(1+1, 2) 
 
+	@unittest.expectedFailure
 	def test_sqrt(self):
 		self.assertEqual(math.sqrt(10)**10, 10)
   
