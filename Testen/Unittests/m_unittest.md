7a27d768e70bbf489c79705579008550426a4441
Abdil-Karim Bakir <abdilkarib03@zedat.fu-berlin.de>
2025-12-01 12:19:52 +0100

    %m_unittest 1.5 h

--- Testen/Unittests/m_unittest.md
+++ Testen/Unittests/m_unittest.md
@@ -2,3 +2,10 @@
 Der Punkt bedeutet: ``Test bestanden``
 
 
+### F2: Was bedeutet offenbar die erste Zeile der Ausgabe?
+Die erste Zeile mit ``.F`` steht für ``Failure``.
+
+
+### F3: Was bedeutet offenbar die Änderung in der ersten Zeile der Ausgabe?
+Die erste Zeile mit ``.X`` steht für ``Expected Failure``.
+
--- Testen/Unittests/test_unittest.py
+++ Testen/Unittests/test_unittest.py
@@ -3,10 +3,16 @@ import math
 
 class Test(unittest.TestCase):
 
+	# A1 
 	def test_addition(self):
 		self.assertEqual(1+1, 2) 
 
-	@unittest.expectedFailure
+	# A2 - A4
+	#@unittest.expectedFailure
 	def test_sqrt(self):
-		self.assertEqual(math.sqrt(10)**10, 10)
-  
+		self.assertAlmostEqual(math.sqrt(10)**2, 10)
+
+	# A5
+	def test_sqrt_of_negative_value(self):
+		with self.assertRaises(ValueError):
+			math.sqrt(-1)
