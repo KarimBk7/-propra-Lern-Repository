3b881209e14cae0de33b75bc5eb4e5a0e3b5cade
Abdil-Karim Bakir <abdilkarib03@zedat.fu-berlin.de>
2025-12-04 11:25:01 +0100

    korrigiert

---
+++ Testen/Unittests/pytest_call.sh
@@ -0,0 +1,21 @@
+pytest tests/test_requests.py
+
+pytest -x tests/test_requests.py
+
+pytest --last-failed
+
+pytest --sw tests/test_requests.py
+
+pytest --sw --sw-skip tests/test_requests.py
+
+pytest --sw --sw-skip tests/test_requests.py
+
+pytest --sw tests/test_requests.py
+
+pytest -vv -rs tests/test_requests.py
+
+pytest -m fixme tests/test_requests.py
+
+pytest -m fixme tests/test_requests.py
+
+pytest -q
